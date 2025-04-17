da64b7d0384bbc84bdf72de6e34bc3e6ddd3d608
Slionkyiu1129 <xinyuu9215@gmail.com>
2025-04-18 03:55:44 +0800

    相機膠卷寬度調整

--- src/components/CameraLine.jsx
+++ src/components/CameraLine.jsx
@@ -1,21 +1,22 @@
 function CameraLine() {
     return (
         <div className="flex flex-col mx-10">
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
-            <div className="border-8 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:border-13 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:hidden rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-8 sm:hidden rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
             <div className="border-8 sm:hidden rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
             <div className="border-8 sm:hidden rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
             <div className="border-8 sm:hidden rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
