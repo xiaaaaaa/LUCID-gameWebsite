1685abd4f1ed84284fed97656c90db4dfb577f08
Slionkyiu1129 <xinyuu9215@gmail.com>
2025-04-17 17:07:28 +0800

    WorldViewPage FirstStage
    Not finished yet.

--- src/pages/WorldviewPage.jsx
+++ src/pages/WorldviewPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav"
 import Footer from "../components/Footer";
+import WorldViewBody from "../components/WorldViewBody";
 
 function WorldviewPage() {
     return (
@@ -13,55 +14,8 @@ function WorldviewPage() {
                         <img src="/images/flowerShopIcon.png" alt="flowerShopIcon" />
                     </Link>
                 </button>
-
-                <div className="flex justify-center items-center">
-                    <div className="grid grid-cols-1 gap-6content">
-                        <div className="card flex flex-col md:flex-row bg-base-100 w-full shadow-sm">
-                            <figure>
-                                <img
-                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                    alt="Shoes" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="card-title">角色介紹</h2>
-                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary">Buy Now</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card flex flex-col md:flex-row-reverse bg-base-100 w-full shadow-sm">
-                            <figure>
-                                <img
-                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                    alt="Shoes" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="card-title">角色介紹</h2>
-                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary">Buy Now</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card bg-base-100 w-full shadow-sm">
-                            <figure>
-                                <img
-                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                    alt="Shoes" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="card-title">地圖</h2>
-                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary">Buy Now</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-
+            <WorldViewBody />
             <Footer />
         </div>
     );
