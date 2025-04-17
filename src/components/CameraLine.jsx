8e96aa6cb05538d7a894dc13fecb0c79ee0e1153
Slionkyiu1129 <xinyuu9215@gmail.com>
2025-04-18 01:36:40 +0800

    WorldViewBody的RWD 大致已完成

---
+++ src/components/CameraLine.jsx
@@ -0,0 +1,23 @@
+function CameraLine() {
+    return (
+        <div className="flex flex-col mx-10">
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+            <div className="border-5 sm:border-17 rounded border-[#D9D9D9] h-[30px] sm:h-[70px] mb-5"></div>
+        </div>
+    )
+}
+export default CameraLine;
