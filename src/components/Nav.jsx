82b7c7fecf4b635fdb84b987c485f7f2265bbb66
xiaaaaaa <78157957+xiaaaaaa@users.noreply.github.com>
2025-06-04 23:35:01 +0800

    漢堡選單動畫

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
-import { useState } from 'react';
-import HamMenu from "./HamMenu";
+import { useState, useRef } from 'react';
+//import HamMenu from "./HamMenu";
+import AnimatedHamMenu from "../motion/AnimatedHamMenu";
 import SetColorMode from "./SetColorMode";
+import { motion } from "framer-motion";
 
 function Nav() {
     const [isOpen, setIsOpen] = useState(false);
+    const containerRef = useRef(null);
     const navBarContent = [
         { to: "/", label: "遊戲介紹" },
         { to: "/world", label: "世界觀" },
@@ -12,17 +15,39 @@ function Nav() {
         { to: "/fanclub", label: "粉絲俱樂部" },
         { to: "/team", label: "製作團隊" },
     ]
+    const menuVariants = {
+        open: {
+            clipPath: "circle(1500px at calc(100% - 40px) 40px)",
+            transition: {
+                type: "spring",
+                stiffness: 15,
+                damping: 10,  
+                duration: 1,  
+                restDelta: 2
+            }
+        },
+        closed: {
+            clipPath: "circle(30px at calc(100% - 40px) 40px)",
+            transition: {
+                delay: 0.2,
+                type: "spring",
+                stiffness: 300,
+                damping: 40
+            }
+        }
+    };
     const NavBarContent = () => (
         <div className="flex flex-col sm:flex-row justify-between sm:space-x-6">
             {navBarContent.map(({ to, label }) => (
-                <div className="my-1">
+                // key 應該放在最外層元素
+                <div className="-my-1 sm:my-1" key={to}>
                     <NavLink
-                        key={to}
                         to={to}
                         onClick={() => setIsOpen(false)}
                         className={({ isActive }) =>
-                            `my-3 sm:my-0 transition-all duration-500 ease-in-out ${isActive ? "opacity-100" : "opacity-60"
-                            } hover:opacity-100`
+                            `my-3 sm:my-0 transition-all duration-500 ease-in-out ${
+                                isActive ? "opacity-100" : "opacity-60"
+                            } hover:opacity-100 hover:scale-115 inline-block`
                         }
                     >
                         {label}
@@ -47,27 +72,37 @@ function Nav() {
             </div>
             {/* 手機版 Drawer */}
             <div className="sm:hidden mobile-nav fixed top-0 w-screen z-50">
-                <div className="flex flex-row justify-between items-center h-16 mr-[70px]">
-                    <NavLink to="/"><img className="h-[45px] pl-5" src="/images/lucidLogo.png" alt="home" /></NavLink>
-                    <div className="flex flex-row items-center">
-                        <div className="mr-[10%]">
-                            <SetColorMode />
+                 <motion.nav
+                    initial={false}
+                    animate={isOpen ? "open" : "closed"}
+                    ref={containerRef}
+                >
+                    <div className="flex flex-row justify-between items-center h-16 -mr-5">
+                        <NavLink to="/">
+                            <img className="h-[45px] pl-5" src="/images/lucidLogo.png" alt="home" />
+                        </NavLink>
+                        <div className="flex flex-row items-center pr-5">
+                            <div className="mr-4">
+                                <SetColorMode />
+                            </div>
+                            <AnimatedHamMenu
+                                toggle={() => setIsOpen(!isOpen)}
+                                isOpen={isOpen}
+                            />
                         </div>
-                        <HamMenu
-                            id="ham-menu"
-                            className="cursor-pointer"
-                            onClick={() => setIsOpen(!isOpen)}
-                            isOpen={isOpen}
-                        />
                     </div>
-                </div>
 
-                {/* dropdown menu，從 HamMenu 下方展開 */}
-                {isOpen && (
-                    <div className="drop left-0 right-0 p-4 z-50">
+                    {/* dropdown menu */}
+                    {isOpen && (<motion.div 
+                        className="drop p-4 z-50 bg-base-100"
+                        variants={menuVariants}
+                        initial="closed"
+                        animate={isOpen ? "open" : "closed"}
+                    >
                         <NavBarContent />
-                    </div>
-                )}
+                    </motion.div>
+                    )}
+                </motion.nav>
             </div>
         </>
     );
