8de79df78d558f6513c429921f2b506c0af606d2
xiaaaaaa <78157957+xiaaaaaa@users.noreply.github.com>
2025-06-05 19:22:31 +0800

    tab 加上 layout animation

--- src/pages/ArtGalleryPage.jsx
+++ src/pages/ArtGalleryPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { motion } from "motion/react";
 import Nav from "../components/Nav"
 import Footer from "../components/Footer";
 import artData from "../json/art.json";
@@ -7,10 +8,11 @@ import ArtPic from "../components/ArtPic";
 import DownfadeInDiv from "../motion/DownfadeInDiv";
 import { selectLightMode } from '../redux/colorSlice';
 import { selectUserHeart } from '../redux/userHeartSlice';
-import { selectWorldHeart } from '../redux/worldHeartSlice'; 
+import { selectWorldHeart } from '../redux/worldHeartSlice';
 
 function ArtGalleryPage() {
-    const [artworks, setArtworks] = useState(artData); 
+    const [selected, setSelected] = useState(0);
+    const [artworks, setArtworks] = useState(artData);
     const worldHeartData = useSelector(selectWorldHeart);
     const sortedArtworks = [...artworks].sort((a, b) => {
         const aHearts = worldHeartData.find(w => w.id === a.id)?.getHeartQty || 0;
@@ -20,7 +22,12 @@ function ArtGalleryPage() {
     const [activeTab, setActiveTab] = useState('all');
     const lightMode = useSelector(selectLightMode);
     const userLovePic = useSelector(selectUserHeart);
-    
+
+    const tabBarContent = [
+        { name: "作品總覽", label: 'all' },
+        { name: "我的收藏", label: 'favorite' },
+    ];
+
 
     return (
         <div className="relative">
@@ -33,55 +40,50 @@ function ArtGalleryPage() {
 
 
             <div className="m-10 mb-8 sm:m-20">
-                <div className="flex gap-2">
-                    <div className="w-1/2">
-                        <button
-                            onClick={() => setActiveTab('all')}
-                            className={`w-full p-0 text-[25px] font-bold text-white rounded-t-[20px]
-                                ${activeTab === 'all'
-                                    ? `bg-white ${lightMode ? '!text-[#E93969]' : '!text-[#30B1BD]'}`
-                                    : `bg-white/20 hover:bg-white/30 ${lightMode ? '!text-[#909090]' : '!text-[#ffffff]'}`}`}
-                        >
-                            <div className="flex flex-col items-center mt-5 gap-5">
-                                <span>作品總覽</span>
-                                <div className={`h-[2px] w-[95%] mt-0 
-                                    ${activeTab === 'all'
-                                        ? (lightMode ? 'bg-[#E93969]' : 'bg-[#30B1BD]')
-                                        : 'bg-transparent'}`}
-                                />
-                            </div>
-                        </button>
-                    </div>
-                    <div className="w-1/2">
-                        <button
-                            onClick={() => setActiveTab('favorite')}
-                            className={`w-full p-0 text-[25px] font-bold text-white rounded-t-[20px]
-                                ${activeTab === 'favorite'
-                                    ? `bg-white ${lightMode ? '!text-[#E93969]' : '!text-[#30B1BD]'}`
-                                    : `bg-white/20 hover:bg-white/30 ${lightMode ? '!text-[#909090]' : '!text-[#ffffff]'}`}`}
-                        >
-                            <div className="flex flex-col items-center mt-5 gap-5">
-                                <span>我的收藏</span>
-                                <div className={`h-[2px] w-[95%] mt-0 
-                                    ${activeTab === 'favorite'
-                                        ? (lightMode ? 'bg-[#E93969]' : 'bg-[#30B1BD]')
-                                        : 'bg-transparent'}`}
-                                />
-                            </div>
-                        </button>
-                    </div>
-                </div>
+                <motion.div className="flex gap-2">
+                    {tabBarContent.map(({ name, label }, index) => (
+                        <div key={index} className="w-1/2">
+                            <button
+                                onClick={() => setActiveTab(label)}
+                                onHoverStart={() => setSelected(index)}
+                                className={`w-full p-0 text-[25px] font-bold text-white rounded-t-[20px]
+                                ${activeTab === label
+                                        ? `bg-white ${lightMode ? '!text-[#E93969]' : '!text-[#30B1BD]'}`
+                                        : `bg-white/20 hover:bg-white/30 ${lightMode ? '!text-[#909090]' : '!text-[#ffffff]'}`}`}
+                            >
+                                <div className="flex flex-col items-center mt-5 gap-5 mb-6">
+                                    <motion.div className="relative">
+                                        <span>{name}</span>
+                                        {activeTab === label && (
+                                            <motion.div
+                                                className={`absolute -bottom-6 left-[-200%] h-[3px] w-[500%] rounded-[6px] 
+                                                ${lightMode ? 'bg-[#E93969]' : 'bg-[#30B1BD]'}`}
+                                                layoutId="tabUnderline"
+                                                transition={{
+                                                    layout: {
+                                                        duration: 0.3,
+                                                        ease: 'easeOut',
+                                                    },
+                                                }}
+                                            />
+                                        )}
+                                    </motion.div>
+                                </div>
+                            </button>
+                        </div>
+                    ))}
+                </motion.div>
 
                 <div className="bg-white p-8 min-h-[600px] rounded-b-[20px]">
                     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                         {activeTab === 'all'
                             ? sortedArtworks.map((art) => (
-                                <ArtPic key={art.id} art={art}/>
+                                <ArtPic key={art.id} art={art} />
                             ))
                             : sortedArtworks
                                 .filter(art => userLovePic.some(item => item.id === art.id))
                                 .map((art) => (
-                                    <ArtPic key={art.id} art={art}/>
+                                    <ArtPic key={art.id} art={art} />
                                 ))
                         }
                     </div>
